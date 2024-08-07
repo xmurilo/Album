4d68acfc8b4a0efdb42f88a75a6cad883988a986
Murilo <dixmurilox@gmail.com>
2024-08-07 17:33:58 -0300

    refactor: Update ListPhotos component to use FunctionComponent and add prop types

--- src/components/ListPhotos.tsx
+++ src/components/ListPhotos.tsx
@@ -1,11 +1,26 @@
+import { FunctionComponent } from "react";
+import Photo from "./Photo";
 
-
-const ListPhotos = () => {
-  return (
-    <div>
-      ListPhotos
-    </div>
-  )
+interface PhotoData {
+  id: string;
+  urls: {
+    small: string;
+  };
+  alt_description: string;
 }
 
-export default ListPhotos
+interface PhotosProps {
+  photos: PhotoData[];
+}
+
+const ListPhotos: FunctionComponent<PhotosProps> = ({ photos }) => {
+  return (
+    <div className="album">
+      {photos.map(photo => (
+        <Photo key={photo.id} data={photo} />
+      ))}
+    </div>
+  );
+};
+
+export default ListPhotos;
