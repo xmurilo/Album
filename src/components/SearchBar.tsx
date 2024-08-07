0d69542241a74e5c3c0c7fbe0ded2e2de1067247
Murilo <dixmurilox@gmail.com>
2024-08-07 17:55:01 -0300

    chore: Update SearchBar component to use FunctionComponent and add prop types

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,24 @@
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+import { FunctionComponent, memo } from "react";
+
+interface SearchBarProps {
+  setCategory: (value: string) => void;
+}
+
+const SearchBar: FunctionComponent<SearchBarProps> = ({ setCategory }) => {
+  const category = ["Natureza", "Pessoas", "Futebol", "Desenhos", "Anime"];
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Pesquisar fotos..." />
+      <button>Pesquisar</button>
+      <select onChange={e => setCategory(e.target.value)}>
+        {category.map(category => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
-export default SearchBar;
+export default memo(SearchBar);
