bc82f4a81f5caa17202a60cd42b89e4b79b50842
Murilo <dixmurilox@gmail.com>
2024-08-08 14:39:34 -0300

    refactor: Update AmplifiedPhoto component to use FunctionComponent and add prop types

--- src/components/AmplifiedPhoto.tsx
+++ src/components/AmplifiedPhoto.tsx
@@ -1,10 +1,19 @@
+import { FunctionComponent, memo } from "react";
+import { PhotoData } from "./ListPhotos";
 
-const AmplifiedPhoto = () => {
-  return (
-    <div>
-      AmplifiedPhoto
-    </div>
-  )
+interface AmplifiedPhotoProps {
+  photo: PhotoData | null;
+  setPhotoAmplified: (value: null) => void;
 }
 
-export default AmplifiedPhoto
+const AmplifiedPhoto: FunctionComponent<AmplifiedPhotoProps> = ({ photo, setPhotoAmplified }) => {
+  return (
+    <div onClick={() => setPhotoAmplified(null)} className="photo-amplified-backdrop">
+      <div className="photo-amplified-container">
+        <img src={photo?.urls.regular} alt={photo?.alt_description} />
+      </div>
+    </div>
+  );
+};
+
+export default memo(AmplifiedPhoto);
