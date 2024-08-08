fe5631e6788f6ceeb740ba8ef4a66533e23aa308
Murilo <dixmurilox@gmail.com>
2024-08-08 14:39:26 -0300

    refactor: Update Photo component to use PhotoData interface and add onClick event handler

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,17 +1,14 @@
 import { FunctionComponent } from "react";
+import { PhotoData } from "./ListPhotos";
 
 interface PhotoProps {
-  data: {
-    urls: {
-      small: string;
-    };
-    alt_description: string;
-  };
+  data: PhotoData 
+  setAmplifiedPhoto: (data: PhotoData) => void;
 }
 
-const Photo: FunctionComponent<PhotoProps> = ({ data }) => {
+const Photo: FunctionComponent<PhotoProps> = ({ data, setAmplifiedPhoto }) => {
   return (
-    <div className="photo">
+    <div className="photo" onClick={() => setAmplifiedPhoto(data)}>
       <img src={data.urls.small} alt={data.alt_description} />
     </div>
   );
