8a1dbd80a621f89acd068921c7bcf5fd9bedf175
Murilo <dixmurilox@gmail.com>
2024-08-08 14:39:20 -0300

    feat: Add onChange event handler to SearchBar input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,13 +2,18 @@ import { FunctionComponent, memo } from "react";
 
 interface SearchBarProps {
   setCategory: (value: string) => void;
+  setQuery: (value: string) => void;
 }
 
-const SearchBar: FunctionComponent<SearchBarProps> = ({ setCategory }) => {
+const SearchBar: FunctionComponent<SearchBarProps> = ({ setCategory, setQuery }) => {
   const category = ["Natureza", "Pessoas", "Futebol", "Desenhos", "Anime"];
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Pesquisar fotos..." />
+      <input
+        type="text"
+        placeholder="Pesquisar fotos..."
+        onChange={e => setQuery(e.target.value)}
+      />
       <button>Pesquisar</button>
       <select onChange={e => setCategory(e.target.value)}>
         {category.map(category => (
